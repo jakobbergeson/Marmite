0b058312c5f70fd283adc1ee7c5ca93e8aba7c36
Jakob Bergeson <jakobbergeson.dev@gmail.com>
2022-04-19 17:17:49 -0700

    Add styled jsx in RecipeCard for more attractive UI.

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -23,7 +23,65 @@ export default function RecipeCard({
           <Link href={`/recipes/${slug}`}><a>Cook This</a></Link>
         </div>
       </div>
+      <style jsx>{`
+        .card {
+          max-width: fit-content;
+          transform: rotateZ(-1deg);
+          transition: transform 200ms ease-in-out, scale 200ms ease-in-out;
+        }
+        .card:hover {
+          transform: rotateZ(0deg) scale(1.1);
+          transition: transform 200ms ease-in-out;
+        }
+        .card:hover .content {
+          transform: translate(10px, 0px);
+          transition: transform 200ms ease-in-out;
+        }
+        .card:hover .actions {
+          transform: translate(-10px, -10px);
+          transition: transform 200ms ease-in-out;
+        }
+        .content {
+          background: #fff;
+          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+          margin: 0;
+          position: relative;
+          top: -40px;
+          left: -10px;
+          transition: all 200ms ease-in-out;
+        }
+        .info {
+          padding: 16px;
+        }
+        .info h4 {
+          margin: 4px 0;
+          text-transform: uppercase;
+        }
+        .info p {
+          margin: 0;
+          color: #777;
+        }
+        .actions {
+          margin-top: 20px;
+          display: flex;
+          justify-content: flex-end;
+          transition: transform 200ms ease-in-out;
+        }
+        .actions a {
+          color: #fff;
+          background: #f01b29;
+          padding: 16px 24px;
+          text-decoration: none;
+          transition: all 400ms ease-in-out;
+        }
 
+        .actions a:hover {
+          background:  #000000;
+          padding: 16px 24px;
+          text-decoration: none;
+          transition: all 200ms ease-in-out;
+        }
+      `}</style>
     </div>
   );
 }
