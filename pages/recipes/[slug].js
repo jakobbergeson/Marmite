f36d07ce70608ba518fc7d05de343bbe386559e5
Jakob Bergeson <jakobbergeson.dev@gmail.com>
2022-04-20 11:57:38 -0700

    Add 404 page with redirect to home after 4 seconds.

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,6 @@
 import { createClient } from "contentful";
 import Image from "next/image";
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-
 import Skeleton from "../../components/Skeleton";
 
 const client = createClient({
